ddef8b2a61dee3994c33e3592672c0b5de6e51c4
Chacko <jasmine.chacko@accenture.com>
2022-08-30 08:04:12 -0400

    Logic for transitioning to screens update -- works now

--- app/src/main/java/com/example/disneycodechallenge_jasminechacko/MainActivity.kt
+++ app/src/main/java/com/example/disneycodechallenge_jasminechacko/MainActivity.kt
@@ -33,8 +33,8 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         createGuestList()
-        val guestsWithReservationsList = createGuestWithReservationList()
-        val guestsWithoutReservationsList = createGuestWithoutReservationList()
+        var guestsWithReservationsList = createGuestWithReservationList()
+        var guestsWithoutReservationsList = createGuestWithoutReservationList()
         guestsWithReservationsAdapter = GuestAdapter(guestsWithReservationsList)
         guestsWithoutReservationsAdapter = GuestAdapter(guestsWithoutReservationsList)
 
@@ -54,7 +54,8 @@ class MainActivity : AppCompatActivity() {
         }
 
         if (savedInstanceState != null) {
-            savedInstanceState.get(guestsWithReservationsList.toString())
+            guestsWithReservationsList = savedInstanceState.get(guestsWithReservationsList.toString()) as MutableList<Guest>
+            guestsWithoutReservationsList = savedInstanceState.get(guestsWithoutReservationsList.toString()) as MutableList<Guest>
         }
 
         binding.btnContinue.setOnClickListener {
@@ -74,9 +75,9 @@ class MainActivity : AppCompatActivity() {
             val intent = if (guestWithReservationCount > 0 && guestWithoutReservationCount == 0) {
                 Intent(this, ConfirmationScreen::class.java)
             } else if (guestWithReservationCount == 0 && guestWithoutReservationCount >= 0) {
-                Intent(this, ConflictScreen::class.java)
-            } else {
                 Intent(this, NoReservationScreen::class.java)
+            } else {
+                Intent(this, ConflictScreen::class.java)
             }
 
             startActivity(intent)
@@ -87,29 +88,31 @@ class MainActivity : AppCompatActivity() {
         // with reservations
         val guest1 = Guest("Viji Chacko", false, true)
         val guest2 = Guest("Shiny Chacko", false, true)
-        val guest3 = Guest("Jensine Joseph", false, true)
-        val guest4 = Guest("Jason Chacko", false, true)
-        val guest5 = Guest("Lynns Chacko", false, true)
-        val guest6 = Guest("Jasmine Chacko", false, true)
-        val guest15 = Guest("Dwight Schrute", false, true)
+//        val guest3 = Guest("Jensine Joseph", false, true)
+//        val guest4 = Guest("Jason Chacko", false, true)
+//        val guest5 = Guest("Lynns Chacko", false, true)
+//        val guest6 = Guest("Jasmine Chacko", false, true)
+//        val guest15 = Guest("Dwight Schrute", false, true)
 
         // without reservations
         val guest7 = Guest("Jeremiah Joseph", false, false)
         val guest8 = Guest("Beckham Joseph", false, false)
-        val guest9 = Guest("Lyla Chacko", false, false)
-        val guest10 = Guest("Pattu Chacko", false, false)
-        val guest11 = Guest("Birdie Chacko", false, false)
-        val guest12 = Guest("Kitty Chacko", false, false)
-        val guest13 = Guest("Simba Joseph", false, false)
-        val guest14 = Guest("Michael Scott", false, false)
-        val guest16 = Guest("Jim Halpert", false, false)
+//        val guest9 = Guest("Lyla Chacko", false, false)
+//        val guest10 = Guest("Pattu Chacko", false, false)
+//        val guest11 = Guest("Birdie Chacko", false, false)
+//        val guest12 = Guest("Kitty Chacko", false, false)
+//        val guest13 = Guest("Simba Joseph", false, false)
+//        val guest14 = Guest("Michael Scott", false, false)
+//        val guest16 = Guest("Jim Halpert", false, false)
 
         allGuests = mutableListOf(
             // with reservations
-            guest1, guest2, guest3, guest4, guest5, guest6, guest15,
+            guest1, guest2,
+                    // guest3, guest4, guest5, guest6, guest15,
 
             // without reservations
-            guest7, guest8, guest9, guest10, guest11, guest12, guest13, guest14, guest16
+            guest7, guest8
+                    // guest9, guest10, guest11, guest12, guest13, guest14, guest16
         )
     }
 
